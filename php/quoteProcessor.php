e25d5e9ea0867617827e0f4d7f446a9c2e51620c
kevinm.reid47 <kevinm.reid47@gmail.com>
2016-12-08 11:33:15 -0500

    message

--- php/quoteProcessor.php
+++ php/quoteProcessor.php
@@ -7,23 +7,7 @@
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
-  <script type="text/javascript">
- var xmlDoc=null;
- if (window.ActiveXObject)
- {// code for IE
-  xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
- }
- if (xmlDoc!=null)
- { 
-  xmlDoc.async=false;xmlDoc.load("/data/quotes.xml");
-  var x=xmlDoc.getElementsByTagName("z:row");
-  var i = Math.floor((Math.random()*1000)%x.length);
-  {
-   document.write(x[i].getElementsByTagName("roy")[0]
-    .childNodes[0].nodeValue);
-  }
- }
-</script>
+  
 </head>
 <body>
 
@@ -35,24 +19,20 @@
   <center>
     <h1> Thank you! </h1> 
     <?php
-    if (isset($_POST['submit'])) {
-    // load previous XML from file
+
       $xml = simplexml_load_file("./data/quotes.xml") or die("ERROR: Cannot load quotes.xml.");
 
      
 
-       echo $xml->roy . "<br>";
-      
+       echo $xml->quote[0] . "<br>";
+      ?>
   
       
       
       
   
-} else {
-      // nothing happened --go back to feedback form
-      header("Location: ../index1.html");
-}
-?>
+
+
   </center>
 </div>
 
